ce3426287a7b587deaf35e387baf825bdfbf71e5
FilipeBmf <lipe.bomfim99@gmail.com>
2018-11-29 23:35:37 -0300

    Atualizado analisador sintatico

--- analisint.c
+++ analisint.c
@@ -6,71 +6,193 @@
 
 
 Token tk ;
+FILE *fp;
 
-void getToken(FILE *fp){
+void getToken(){
 	tk=AnalisadorLexico(fp);
 }
 
 void Erro(int e) {
+	if(e>0) system("color f4");
+	else system("color f2");
 	printf("Erro: %s\n", erros[e]);
 	exit(e);
 }
 
-void prog(FILE *fp){
-	getToken(fp);
+void prog(){
+	getToken();
 	if((tk.cat==PR)&&(tk.valor.numInt==PL)){
 	
 	getToken(fp);
 	if(tk.cat==ID){
-		getToken(fp);
+		getToken();
 		if((tk.cat==PR)&&(tk.valor.numInt==VAR)){
-			getToken(fp);
+			getToken();
 			while(tk.valor.numInt!=ENDVAR){
-				tipo(fp);
-				getToken(fp);
-				decl_var(fp);
-				getToken(fp);
+				tipo();
+				getToken();
+				decl_var();
+				getToken();
 				while(tk.valor.numInt==VIRG){
-					getToken(fp);
-					decl_var(fp);
-					getToken(fp);
-				}
-				
+					getToken();
+					decl_var();
+					getToken();
+				}				
 			}
-
-			
 		}
+		/*
+		while((tk.cat==PR)&&(tk.valor.numInt!=PROG)){
+			if((tk.cat==PR)&&(tk.valor.numInt==FWD) {
+				getToken(fp);
+				if((tk.cat==PR)&&(tk.valor.numInt==PROC) proc(fp);
+			}
+			if((tk.cat==PR)&&(tk.valor.numInt==PROC) proc(fp);
+		}
+		*/		
+		
+		
 	}else{
-		// ERRO ID - Erro()
+		Erro(7);
 	}
 	
 	}else {
-		// ERRO PL - Erro()
+		Erro(7);
 	}
 	
-	Erro(0);
+	
 }
 							
-void decl_var(FILE *fp){
-	if(tk.cat!=ID) Erro(4); 
+void decl_var(){
+	if(tk.cat!=ID) Erro(7); 
 }		
 
-void tipo(FILE *fp){
+void tipo(){
 	if(tk.cat==PR){
-		if(!((tk.valor.numInt==CHAR) || (tk.valor.numInt==INT) || (tk.valor.numInt==REAL) || (tk.valor.numInt==BOOL))) Erro(4);
+		if(!((tk.valor.numInt==CHAR) || (tk.valor.numInt==INT) || (tk.valor.numInt==REAL) || (tk.valor.numInt==BOOL))) Erro(7);
 	}else {
-		Erro(4);
+		Erro(7);
 	}	
 }
 
+void fwd(){
+     getToken();
+     if((tk.cat==PR)&&(tk.valor.numInt==PROC)){
+         getToken();                                                                     
+     }else{
+         tipo();
+         getToken();
+     } 
+     if(tk.cat==ID){
+          getToken();
+          if((tk.cat==DELIMITADOR)&&(tk.valor.numInt==A_PARENT)){
+                getToken();
+                if((tk.cat==PR)&&(tk.valor.numInt==NOPARAM)){
+                      getToken();                                      
+                }else{
+                      tipo();
+                      getToken();
+                      while(tk.valor.numInt==VIRG){
+                          getToken();
+					      tipo();
+					      getToken();
+				      }
+                } 
+                if((tk.cat!=DELIMITADOR)&&(tk.valor.numInt!=F_PARENT)) Erro(4);
+                                                           
+          }else Erro(3);         
+     }else{
+           Erro(7);
+     }                                                                                   
+}	
 
+void func(){
+	getToken();
+	tipo();
+	getToken();
+	if(tk.cat==ID){
+		getToken();
+		if((tk.cat!=DELIMITADOR)&&(tk.valor.numInt!=A_PARENT)){
+			Erro(3);	
+		}else {
+			tipos_param();
+			if((tk.cat!=DELIMITADOR)&&(tk.valor.numInt!=F_PARENT)){
+				Erro(4);
+			}else{
+				getToken();
+				while((tk.valor.numInt!=ENDFUNC)&&
+					 (tk.valor.numInt!=IF) &&
+					 (tk.valor.numInt!=WHILE) &&
+					 (tk.valor.numInt!=FOR) &&
+					 (tk.valor.numInt!=RETURN) &&
+					 (tk.valor.numInt!=CALL) &&
+					 (tk.valor.numInt!=PT_VIRG) &&
+					 (tk.valor.numInt!=KEYBOARD) &&
+					 (tk.valor.numInt!=DISPLAY) &&
+					 (tk.cat!=ID)) {
+					
+						tipo();
+						getToken();
+						decl_var();
+						getToken();
+						while(tk.valor.numInt==VIRG){
+							getToken();
+							decl_var();
+							getToken();
+						}				
+				}
+				
+				while(tk.valor.numInt!=ENDFUNC){
+				//	cmd();
+					getToken();
+				}
+				
+			}
+		}
+	}else{
+		Erro(7);
+	}
+}	
 
-int analisint(FILE *fp){
+void proc(){
 	
 }
 
+void atrib(){
+	getToken();
+	if(!(tk.cat==ID))Erro(7);
+	getToken();
+	if(!((tk.cat==OPREL)&&(tk.valor.numInt==IGUAL)))Erro(7);
+	//expr(fp);
+}
+
+void oprel(){
+	getToken();
+	if(!(tk.cat==OPREL)) Erro(7);
+	if(!((tk.valor.numInt==IGUALIGUAL)||(tk.valor.numInt==DIFERENTE)||(tk.valor.numInt==MENORIGUAL)||(tk.valor.numInt==MAIORIGUAL)||
+	(tk.valor.numInt==MENORIGUAL)||(tk.valor.numInt==MENOR)||(tk.valor.numInt==MAIOR))) Erro(7);
+}
+
+void tipos_param(){
+	getToken();
+	if(tk.cat!=PR) Erro(7);
+	if(tk.valor.numInt!=NOPARAM){
+		tipo();
+		getToken();
+		if(tk.cat!=ID) Erro(7);
+		getToken();
+		while(tk.valor.numInt==VIRG){
+				getToken();
+				tipo();
+				getToken();
+				if(tk.cat!=ID) Erro(7);
+				getToken();
+		}
+		
+	}
+}
+
 int main(int argc, char *argv[]) {
-	FILE *fp;
+	
 	int linha=0; 
 	
 	printf("[ANALEX LINGUAGEM PL]\n\n");
@@ -78,9 +200,9 @@ int main(int argc, char *argv[]) {
 	system("color f0");
 
 	if((fp = fopen("Editor Linguagem PL.txt","r"))==NULL) printf("Arquivo nao pode ser aberto\n"); // VALIDANDO A ABERTURA DO ARQUIVO 
-	prog(fp);
-	printf("\n%d.  ",linha);
-
+	func();
+	Erro(0);
+	
 
 	fclose(fp);
 	
--- analisint.h
+++ analisint.h
@@ -1,12 +1,20 @@
 char erros[][50] = {
-	"Sem erro",
-	"Caracter invalido!",
-	"Formato de numero invalido!",
-	"Faltando fecha parenteses!",
-	"Tipo de Variavel Invalida",
-	"Operando invalido!"};
+	"Sem erro", //Erro 0
+	"Caracter invalido!", //Erro 1
+ 	"Formato de numero invalido!", //Erro 2
+	"Faltando abre parenteses!", //Erro 3
+	"Faltando fecha parenteses!", //Erro 4
+	"Faltando abre colchetes!", //Erro 5
+	"Faltando fecha colchetes!", //Erro 6
+	"Token esperado invalido", //Erro 7
+	"Operando invalido!"}; //Erro 8
 	
 
-void decl_var(FILE *fp);
-void tipo(FILE *fp);
-void getToken(FILE *fp);
+void decl_var();
+void tipo();
+void getToken();
+void proc();
+void fwd();
+void atrib();
+void oprel();
+void tipos_param();
